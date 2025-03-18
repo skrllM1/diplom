f05725f35887ff1da00365a01fe94d15ce961cb9
skrllM1 <108575162+skrllM1@users.noreply.github.com>
2025-03-18 11:10:58 +0300

    Add files via upload

---
+++ shared/s3/js/cookie.js
@@ -0,0 +1,38 @@
+function createCookie(name,value,days,options = {})
+{
+	if (days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+  var updatedCookie = name+"="+encodeURIComponent(value)+expires+"; path=/";
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+	document.cookie = updatedCookie;
+}
+
+function readCookie(name)
+{
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name)
+{
+	createCookie(name,"",-1);
+}
